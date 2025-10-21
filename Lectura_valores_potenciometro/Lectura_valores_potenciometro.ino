ea8936f0525875b32ea7b578a20c21d743733b21
jorgeizaguirree <jorge@izaguirre.eu>
2025-10-21 22:12:19 +0200

    diferentes lecturas

--- Lectura_valores_potenciometro/Lectura_valores_potenciometro.ino
+++ Lectura_valores_potenciometro/Lectura_valores_potenciometro.ino
@@ -1,5 +1,4 @@
 int valor;
-int fi;
 #define sbi(port, bit) (port) |= (1 << (bit))
 #define cbi(port, bit) (port) &= ~(1 << (bit))
 // the setup function runs once when you press reset or power the board
@@ -13,8 +12,6 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  fi = 10;
-  int boton = 5;
   valor = analogRead(A1);
   Serial.print("El potenciometro está dando valor: ");
   Serial.print(valor);
--- lectura_paso_por_cero/lectura_paso_por_cero.ino
+++ lectura_paso_por_cero/lectura_paso_por_cero.ino
@@ -1,6 +1,8 @@
 int valor;
-#define sbi(port,bit)   (port) |= (1 << (bit))
-#define cbi(port,bit)   (port) &= ~(1 << (bit))
+int prev_valor;
+int count;
+#define sbi(port, bit) (port) |= (1 << (bit))
+#define cbi(port, bit) (port) &= ~(1 << (bit))
 // the setup function runs once when you press reset or power the board
 void setup() {
   Serial.begin(57600);
@@ -11,24 +13,49 @@ void setup() {
   sbi(DDRB, DDB6);
 }
 
+
+int convertir(int a) {
+  if (a > 900) {
+    return 1;
+  }
+  if (a < 200) {
+    return 0;
+  }
+  return -1;
+}
+
 // the loop function runs over and over again forever
 void loop() {
-
+  /*
   Serial.println("Enciendo el led");
   digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
   sbi(PORTD, PORTD1);
   sbi (PORTB, PORTB6);
   valor = analogRead(A1);
   Serial.print("El Paso Por Cero está dando valor: ");
-  Serial.print(valor);
-  delay(1000);                      // wait for a second
+  Serial.println(valor);
+  delay(40);                      // wait for a second
   Serial.println("Apago el led");
   digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
   cbi(PORTD, PORTD1);
   cbi(PORTB, PORTB6);
   valor = analogRead(A1);
   Serial.print("El Paso Por Cero está dando valor: ");
-  Serial.print(valor);
-  delay(1000);                      // wait for a second
-  
+  Serial.println(valor);
+  delay(6);                      // wait for a second
+  */
+  int prev_time = millis();
+  while (millis() - prev_time < 1000) {
+    valor = convertir(analogRead(A1));
+    if (valor != -1) {
+      if (valor != prev_valor) {
+        count++;
+      }
+      prev_valor = valor;
+    }
+  }
+  Serial.print("Ha habido ");
+  Serial.print(count);
+  Serial.println(" pasos por cero");
+  count = 0;
 }
